f8501de203bcd9644d050cb60becba34ab6fae7b
Vadym Hurnik <vad.com.z@gmail.com>
2023-12-06 11:15:00 +0200

    added BookController, BookDto, BookMapper and other small changes

--- src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
@@ -1,23 +1,21 @@
 package com.example.bookstore.repository.impl;
 
+import com.example.bookstore.exception.EntityNotFoundException;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory factory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory factory) {
-        this.factory = factory;
-    }
 
     @Override
     public Book save(Book book) {
@@ -32,7 +30,7 @@ public class BookRepositoryImpl implements BookRepository {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't add a new book: " + book, e);
+            throw new EntityNotFoundException("Can't add a new book: " + book, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -47,7 +45,17 @@ public class BookRepositoryImpl implements BookRepository {
             Query<Book> books = session.createQuery("FROM Book b", Book.class);
             return books.getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can't get all books.", e);
+            throw new EntityNotFoundException("Can't get all books.", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (Session session = factory.openSession()) {
+            Book book = session.find(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Cant get book by id: " + id, e);
         }
     }
 }
