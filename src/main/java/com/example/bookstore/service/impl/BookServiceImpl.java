46fe6d00a2436ad6e4868d96475f6c49d5ed92d9
Vadym Hurnik <vad.com.z@gmail.com>
2023-12-12 16:31:45 +0200

    added update and delete(soft) methods

--- src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -37,4 +37,16 @@ public class BookServiceImpl implements BookService {
         );
         return bookMapper.toDto(book);
     }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        book.setId(id);
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
