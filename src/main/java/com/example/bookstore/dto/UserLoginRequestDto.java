aa4cf358dd6cacea586c516070015ac33af18051
Vadym Hurnik <vad.com.z@gmail.com>
2023-12-24 17:22:59 +0200

    added JWT support

---
+++ src/main/java/com/example/bookstore/dto/UserLoginRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UserLoginRequestDto {
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/example/bookstore/dto/UserLoginResponseDto.java
@@ -0,0 +1,8 @@
+package com.example.bookstore.dto;
+
+import lombok.Data;
+
+@Data
+public class UserLoginResponseDto {
+    private String token;
+}
