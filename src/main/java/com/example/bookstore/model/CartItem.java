d9d49102b8f1d6db872a19ee7e7a66efb9e12ebe
Vadym Hurnik <vad.com.z@gmail.com>
2024-01-03 23:20:40 +0200

    changed fetch type

--- src/main/java/com/example/bookstore/model/CartItem.java
+++ src/main/java/com/example/bookstore/model/CartItem.java
@@ -2,6 +2,7 @@ package com.example.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,12 +28,12 @@ public class CartItem {
     private Long id;
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
     @Column(nullable = false)
--- src/main/java/com/example/bookstore/model/ShoppingCart.java
+++ src/main/java/com/example/bookstore/model/ShoppingCart.java
@@ -2,6 +2,7 @@ package com.example.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,7 +31,7 @@ public class ShoppingCart {
     private Long id;
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
     @ToString.Exclude
