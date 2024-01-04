d9283b4653d113c30f2e89f5c1071fb1b9d09009
Vadym Hurnik <vad.com.z@gmail.com>
2024-01-04 17:27:23 +0200

    added annotation to UpdateOrderStatusRequestDto

--- src/main/java/com/example/bookstore/dto/UpdateOrderStatusRequestDto.java
+++ src/main/java/com/example/bookstore/dto/UpdateOrderStatusRequestDto.java
@@ -1,9 +1,11 @@
 package com.example.bookstore.dto;
 
 import com.example.bookstore.model.Order;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateOrderStatusRequestDto {
+    @NotBlank
     private Order.Status status;
 }
