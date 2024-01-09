5689914960e7061a91ff03dafcc9cf7e93e065ed
Vadym Hurnik <vad.com.z@gmail.com>
2024-01-09 14:10:48 +0200

    added test for BookServiceImpl, BookRepository, BookController

---
+++ src/test/resources/database/books/add-books-to-books-table.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (id, title, author, isbn, price, description, cover_image) VALUES (1, 'Java 23', 'Alice Evans', '9817265923', 3.99, 'Java best practices', 'Java23.img');
+INSERT INTO books (id, title, author, isbn, price, description, cover_image) VALUES (2, 'Oceans', 'Bob Carter', '8127561232', 9.25, 'Nature', 'Oceans.img');
+INSERT INTO books (id, title, author, isbn, price, description, cover_image) VALUES (3, 'Apollo', 'Niel Armstrong', '7561928487', 12.75, 'Space journey', 'Moon.img');
